21a1a1344d5c7a05d50e22a56b689c569ddf5ee1
huizzz10 <dinghuiliao4@gmail.com>
2026-06-16 13:09:05 +0800

    同步1

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -99,18 +99,12 @@ export const siteConfig: SiteConfig = {
 	pages: {
 		// 友链页面开关
 		friends: true,
-<<<<<<< HEAD
 		// 赞助页面开关
 		sponsor: false,
-=======
-		// 打赏页面开关
-		sponsor: true,
->>>>>>> upstream/master
 		// 留言板页面开关，需要配置评论系统
 		guestbook: false,
 		// 番组计划页面开关，含追番、游戏、书籍和音乐，dev调试时只获取一页数据，build才会获取全部数据
 		bangumi: false,
-
 		// 相册页面开关
 		gallery: true,
 	},
