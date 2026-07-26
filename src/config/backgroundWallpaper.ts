c9efd8718feb6ecfd41ffb9289b3d45dd033abfa
huizzz10 <dinghuiliao4@gmail.com>
2026-07-26 23:51:24 +0800

    bar

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -75,17 +75,12 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 是否启用主页横幅文字
 			enable: true,
 			// 主页横幅主标题
-			title: "Lovely firefly!",
+			title: "huizzz10's blog",
 			// 主页横幅主标题字体大小
 			titleSize: "4.5rem",
 			// 主页横幅副标题
 			subtitle: [
-				"In Reddened Chrysalis, I Once Rest",
-				"From Shattered Sky, I Free Fall",
-				"Amidst Silenced Stars, I Deep Sleep",
-				"Upon Lighted Fyrefly, I Soon Gaze",
-				"From Undreamt Night, I Thence Shine",
-				"In Finalized Morrow, I Full Bloom",
+				"悟已往之不谏，知来者之可追!"
 			],
 			// 主页横幅副标题字体大小
 			subtitleSize: "1.5rem",
